620871ac4f44f6457bdac481fb560278156c6a9f
rictorlome <rictorlome@gmail.com>
2018-04-15 13:13:34 -0400

    Sketch out binary min heap

---
+++ min_heap.rb
@@ -0,0 +1,106 @@
+require 'byebug'
+
+class Node
+  attr_accessor :key, :val
+  def initialize(key, val)
+    @key = key
+    @val = val
+  end
+end
+
+A_Node = Node.new('A', -1)
+B_Node = Node.new('B', 2)
+C_Node = Node.new('C', 6)
+D_Node = Node.new('D', 4)
+E_Node = Node.new('E', 5)
+F_Node = Node.new('F', 7)
+G_Node = Node.new('G', 8)
+
+#             A,-1
+#           /      \
+#         B,2      C,6
+#        /  \      /   \
+#     D,4  E,5   F,7   G,8
+
+class MinBinaryHeap
+  attr_reader :nodes
+
+  def initialize(nodes=[])
+    @nodes = nodes
+    @map = create_map
+  end
+
+  def create_map
+    map = Hash.new
+    @nodes.each_with_index do |node, idx|
+      map[node.key] = idx
+    end
+    map
+  end
+
+  def left_child(key)
+    i = @map[key]
+    @nodes[2*i + 1]
+  end
+
+  def right_child(key)
+    i = @map[key]
+    @nodes[2*i + 2]
+  end
+
+  def parent(key)
+    i = @map[key]
+    return nil if (i-1)/2 < 0
+    @nodes[(i-1)/2]
+  end
+
+  def add(key,val)
+    throw 'Already in Heap' if self.contains?(key)
+    node = Node.new(key,val)
+    @nodes << node
+    @map[key] = @nodes.length - 1
+    until parent(node.key) == nil || parent(node.key).val <= node.val
+      swap_child_parent(node,parent(node.key))
+    end
+  end
+
+  def contains?(key)
+    !!@map[key]
+  end
+
+  def decrease_val(key,new_val)
+    i = @map[key]
+    node = @nodes[i]
+    node.val = new_val
+    until parent(node.key) == nil || parent(node.key).val <= node.val
+      swap_child_parent(node,parent(node.key))
+    end
+  end
+
+  def swap_child_parent(child,parent)
+    @nodes[@map[child.key]], @nodes[@map[parent.key]] = @nodes[@map[parent.key]], @nodes[@map[child.key]]
+    @map[child.key], @map[parent.key] = @map[parent.key], @map[child.key]
+  end
+
+  def smaller_child(node)
+    left, right = self.left_child(node.key), self.right_child(node.key)
+    return nil if left == nil && right == nil
+    return left if left != nil && right == nil
+    return right if right != nil && left == nil
+    left.val <= right.val ? left : right
+  end
+
+  def extract_min
+    min = @nodes[0]
+    until self.smaller_child(min) == nil
+      nxt = self.smaller_child(min)
+      self.swap_child_parent(nxt,min)
+    end
+    idx = @map[min.key]
+    @map.delete(min.key)
+    @nodes[idx] = nil
+    min
+  end
+end
+
+EX_HEAP = MinBinaryHeap.new([A_Node, B_Node, C_Node, D_Node, E_Node, F_Node, G_Node])
